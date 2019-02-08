ac2388089b92d9d4f03d30535602fbb573353aaf
William Robert Simpson <will.simpson85@gmail.com>
2019-02-08 18:39:11 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,9 +2,13 @@
 def run_guessing_game
   user_input = gets.chomp
 puts "Guess a number between 1 and 6."
+randomNumber = rand(1..7)
 case user_input
 when "exit"
   puts "Goodbye!"
+when user_input == randomNumber.to_st
+  puts "You guessed the correct number!"
+  
 end
 
 end
