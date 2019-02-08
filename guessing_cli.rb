587b213b37d2cb39a7b28690bc2575577fce83d7
William Robert Simpson <will.simpson85@gmail.com>
2019-02-08 19:02:05 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,16 +2,12 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_input = gets.chomp
-
 randomNumber = rand(1..6)
 compare = randomNumber.to_s.chomp
-puts compare.is_a?(String)
-puts compare
-puts user_input.is_a?(String)
-case compare
+case user_input
 when "exit"
   puts "Goodbye!"
-when compare === user_input
+when == compare
   puts "You guessed the correct number!"
 else
   puts "The computer guessed #{randomNumber}"
