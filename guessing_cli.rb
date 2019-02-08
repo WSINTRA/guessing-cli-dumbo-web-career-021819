f0d550b22eb5ce4dc8cc0db3c01a65364eae2951
William Robert Simpson <will.simpson85@gmail.com>
2019-02-08 18:39:24 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ randomNumber = rand(1..7)
 case user_input
 when "exit"
   puts "Goodbye!"
-when user_input == randomNumber.to_st
+when user_input == randomNumber.to_s
   puts "You guessed the correct number!"
 
 end
